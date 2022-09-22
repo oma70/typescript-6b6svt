b2016ab53e1a81efa7a7a7fa867d662c5fcd9409
oma70 <114165957+oma70@users.noreply.github.com>
2022-09-22 15:19:06 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+console.long("hey bb i louve uu ");
